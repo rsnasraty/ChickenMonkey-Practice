40cb4b295cc4ad7601a8513802d55772f311997f
Roxanne Nasraty <rsnasraty@gmail.com>
2020-04-22 21:13:52 -0500

    Finished

---
+++ main.js
@@ -0,0 +1,22 @@
+/* 
+Write a program that prints the numbers from 1 to 100. 
+
+But for multiples of five (5, 10, 15, etc.) print "Chicken" instead of the number  
+
+for the multiples of seven (7, 14, 21, etc.) print "Monkey". 
+
+For numbers which are multiples of both five and seven print "ChickenMonkey".
+
+*/
+
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+    if (currentNumber % 5 === 0 && currentNumber % 7 === 0) {
+        console.log("ChickenMonkey");
+    } else if (currentNumber % 7 === 0) {
+        console.log("Monkey");
+    } else if (currentNumber % 5 === 0) {
+        console.log("Chicken");
+    } else {
+        console.log(currentNumber);
+    }
+}
